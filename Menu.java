d6b15a4bfeb7c120b5b4549c648c4b27abf3457f
Diego Flores <95269690+DiegoF001@users.noreply.github.com>
2022-02-04 01:37:09 -0800

    added Menu and commens

---
+++ Menu.java
@@ -0,0 +1,10 @@
+public class Menu {
+    /* -Menu options
+       -User will exit program with !q
+       -Empty entries, !help keyword, and words that do not exist
+        in the dictionary will display a guide
+
+    */
+
+
+}
